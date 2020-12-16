6fbabcc1f5bf93e57abcd195a92df55aad56c1cf
Tarlan Karimli <terlankerimli@gmail.com>
2020-12-17 01:38:13 +0400

    level arrow added

--- js/play/play.js
+++ js/play/play.js
@@ -1,77 +1,96 @@
 import {showList} from './achievement.js';
 import {countdown, timer} from '../countdown.js';
-import {easy} from '../questions/questionDB.js';
+import {easy, medium, hard} from '../questions/questionDB.js';
+
+
+let levelArrow = 0;
+let questionTypePointer;
+const selectAnswer = document.querySelectorAll('.answer');
 
 document.addEventListener('DOMContentLoaded', ()=> {
-render()
+    render()
 })
 
 const render = () => {
     showList()
     countdown()
-    const levelListItem = document.querySelector('#item-0');
-    levelListItem.style.background = '#FFC107';
-    
-
+    const levelListItem = document.querySelector(`#item-${levelArrow}`);
+    levelListItem.classList.add('selected')
+    // levelListItem.style.background = '#FFC107';
+    levelQuestion();
+    addAnswerEvents();
 //     var a = easy
 // var res = a.sort(function() {
 //   return 0.5 - Math.random();
 // });
 // console.log(res.slice(a,1))
 // console.log(Math.floor(Math.random() * easy.length));
-
 }
 
 
+//  display questions
+const showQuestion = (questionType) => {
+const randomQuestion = Math.floor(Math.random() * questionType.length);
 
-const showQuestion = (level) => {
-const questionContent = document.querySelector('.question');
-const itemA = document.querySelector('.itemA');
-const itemB = document.querySelector('.itemB');
-const itemC = document.querySelector('.itemC');
-const itemD = document.querySelector('.itemD');
+    const questionContent = document.querySelector('.question');
+    const itemA = document.querySelector('.itemA');
+    const itemB = document.querySelector('.itemB');
+    const itemC = document.querySelector('.itemC');
+    const itemD = document.querySelector('.itemD');
 
-questionContent.textContent = level[0].question;
-itemA.textContent = level[0].a;
-itemB.textContent = level[0].b;
-itemC.textContent = level[0].c
-itemD.textContent = level[0].d;
-}
-let levelArrow = 1;
-
-switch (levelArrow) {
-    case 1:
-        showQuestion(easy)
-        break;
-
-    default:
-        break;
+    questionContent.textContent = questionType[randomQuestion].question;
+    itemA.textContent = questionType[randomQuestion].a;
+    itemB.textContent = questionType[randomQuestion].b;
+    itemC.textContent = questionType[randomQuestion].c
+    itemD.textContent = questionType[randomQuestion].d;
 }
 
-const myFunc =(e)=> {
-    let id;
-    if(e.target.tagName === 'SPAN') {
-         e.target.parentElement.style.background = '#ffc10786';
-         id = e.target.parentElement.id;
-}  else {
-        e.target.style.background = '#ffc10786';
-        id = e.target.id;
+// level of question
+const levelQuestion = () => {
+    switch (true) {
+        case levelArrow < 6:
+            showQuestion(easy);
+            questionTypePointer = easy;
+            break;
+        case levelArrow < 11:
+            showQuestion(medium)
+            questionTypePointer = medium;
+            break;
+            case levelArrow < 16:
+            showQuestion(hard)
+            questionTypePointer = hard;
+            break;
+        default:
+            break;
     }
-    checkAnswer(id);
 }
-const selectAnswer = document.querySelectorAll('.answer');
-selectAnswer.forEach(item => {
-    item.addEventListener('mousedown', myFunc)
-    item.addEventListener('mouseup', ()=> {
-        selectAnswer.forEach(e => {
-            e.removeEventListener('mousedown', myFunc)
+//  adding events to answers
+const addAnswerEvents = () => {
+    const myFunc =(e)=> {
+        let id;
+        if(e.target.tagName === 'SPAN') {
+             e.target.parentElement.style.background = '#ffc10786';
+             id = e.target.parentElement.id;
+    }  else {
+            e.target.style.background = '#ffc10786';
+            id = e.target.id;
+        }
+        checkAnswer(id);
+    }    
+    selectAnswer.forEach(item => {
+        item.addEventListener('mousedown', myFunc)
+        item.addEventListener('mouseup', ()=> {
+            selectAnswer.forEach(e => {
+                e.removeEventListener('mousedown', myFunc)
+            })
         })
     })
-})
-
+}
+//  check answer
 const checkAnswer = (id) => {
-    if(id === easy[0].rightAnswer) {
-        console.log("yessss")
+    if(id === questionTypePointer[levelArrow].rightAnswer) {
+        levelArrow++;
+        render();
         return
     } 
     console.log("noooo")
